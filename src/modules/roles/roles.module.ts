de401a548ee2118fdb2ade8c97c51b30d9cda2d5
mubasshir-cyber <mubasshirfarooqui208@gmail.com>
2026-07-21 16:32:08 +0530

    done

--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -5,12 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
 import { Role } from './entities/role.entity';
+import { RBACInitializerService } from './services/rbac-initializer.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Role])],
   controllers: [RolesController],
-  providers: [RolesService],
-  exports: [TypeOrmModule],
+  providers: [RolesService, RBACInitializerService],
+  exports: [TypeOrmModule, RBACInitializerService],
 })
 export class RolesModule { }
-
